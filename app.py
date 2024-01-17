3f6fc2931cee6143ba783d3943f7e12eca7037e6
Brando <bbaker.sh@gmail.com>
2024-01-16 20:30:24 -0800

    add logic for question route

--- app.py
+++ app.py
@@ -4,12 +4,9 @@ from surveys import satisfaction_survey as survey
 app = Flask(__name__)
 
 
-@app.route("/" , methods=["POST"])
+@app.route("/" )
 def hello():
-    
-    responses = []
-    
-    return render_template("survey.html", responses=responses)
+    return render_template("survey.html")
 
 @app.route("/questions/<int:question_id>")
 def questions(question_id):
@@ -17,5 +14,6 @@ def questions(question_id):
         return render_template("invalid.html")
     
     questions = survey.questions[question_id].question
-    
-    return render_template("question.html", question=questions)
+    choices = survey.questions[question_id].choices
+    add_text = survey.questions[question_id].allow_text
+    return render_template("question.html", questions=questions , choices=choices , add_text=add_text)
